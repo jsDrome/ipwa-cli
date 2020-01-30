b31258775d6001ec129837b6d4116a80056cbc30
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-01-30 21:53:31 +0100

    feat: webpack-dev-server

--- build/client/dev.js
+++ build/client/dev.js
@@ -1,6 +1,12 @@
 const merge = require('webpack-merge');
 const commonConfig = require('./common');
+const path = require('path');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '../../_dist'),
+    compress: true,
+    port: 9000,
+  },
 });
