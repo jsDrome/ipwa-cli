486abbea6986fb6f0fb370e4db2cb4378e017c92
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-02-01 23:32:05 +0100

    chore: update to output

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(express.static('_dist'));
 
-app.get('/hi', (req, res) => {
-  res.send('hii');
+app.get('/server', (req, res) => {
+  res.send('Server Side');
 });
 
 app.listen(port, () => console.log(`SERVER: Listening on port ${port}`));
