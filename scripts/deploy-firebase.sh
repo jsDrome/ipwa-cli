cc7fd2ef1084c21612588c963df131c042b6434a
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-02-17 23:27:09 +0100

    chore: moves optional deps to dev deps

--- scripts/deploy-firebase.sh
+++ scripts/deploy-firebase.sh
@@ -3,7 +3,7 @@ npm run build:client:prod
 npm run build:server:firebase
 node ./scripts/package-copier.js
 cd _dist
-npm i --production --no-optional
+npm i --production
 cd ..
 npm run firebase:deploy
 npm run clean
