044fb22282f1a7244b1d1ed6d8308a050dd808cd
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-02-18 17:52:27 +0100

    fix: fixes cli script

--- cli.sh
+++ cli.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
 ARG=$1
-CURRENT_VERSION=`node -p "require('./package.json').version"`
 
 if [ "$1" = "" ]; then
   echo 'No folder name provided!'
   exit;
-elif [ "$1" = "-v" ]; then
-  echo $CURRENT_VERSION
-  exit;
+# elif [ "$1" = "-v" ]; then
+#   echo $CURRENT_VERSION
+#   exit;
 fi
-
-FOLDER=${ARG:-"my-jsdrome-app"}
-wget -c https://codeload.github.com/jsDrome/jsDrome/tar.gz/v$CURRENT_VERSION -O - | tar -xz
-mv jsDrome-$CURRENT_VERSION $FOLDER
+mkdir $1
+wget -c https://github.com/jsDrome/jsDrome/tarball/master -O - | tar -xz - -C "$1" --strip-components=1
