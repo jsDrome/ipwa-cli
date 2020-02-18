12b69faaeb08b7ca5c607131f728040969bd22a4
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-02-18 18:29:26 +0100

    fix: fixes cli.js

---
+++ cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+/* eslint-disable no-magic-numbers */
+
+const { exec } = require('child_process');
+const folderName = process.argv.slice(2)[0] || 'jsdrome';
+
+exec(`mkdir ${folderName} && wget -c https://github.com/jsDrome/jsDrome/tarball/master -O - | tar -xz - -C ${folderName} --strip-components=1`, (err, stdout, stderr) => {
+  if (err) {
+    //some err occurred
+    console.error(err)
+  } else {
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  }
+});
