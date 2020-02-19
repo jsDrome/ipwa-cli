1948997bbcd0c508e92f2846c3dcc2c507c130f5
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-02-19 11:45:47 +0100

    chore: update to cli

--- cli.js
+++ cli.js
@@ -2,6 +2,8 @@
 /* eslint-disable no-magic-numbers */
 
 const { exec } = require('child_process');
+const fs = require('fs');
+
 const folderName = process.argv.slice(2)[0] || 'jsdrome';
 
 exec(`mkdir ${folderName} && wget -c https://github.com/jsDrome/jsDrome/tarball/master -O - | tar -xz - -C ${folderName} --strip-components=1`, (err, stdout, stderr) => {
@@ -9,8 +11,22 @@ exec(`mkdir ${folderName} && wget -c https://github.com/jsDrome/jsDrome/tarball/
     //some err occurred
     console.error(err)
   } else {
+    overWritePackage();
     // the *entire* stdout and stderr (buffered)
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
   }
 });
+
+const overWritePackage = () => {
+  // exec(`rm ${folderName}/package-lock.json`);
+  // eslint-disable-next-line global-require
+  const packageJson = require(`./${folderName}/package.json`);
+
+  packageJson.name = folderName;
+  packageJson.version = '1.0.0';
+  packageJson.homepage = 'https://jsdrome.com';
+  delete packageJson.bugs;
+
+  fs.writeFileSync(`${folderName}/package.json`, JSON.stringify(packageJson, null, "  "));
+};
