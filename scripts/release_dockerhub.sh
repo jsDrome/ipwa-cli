5798d78111b8f6337aa8cc7667d1ba5aa699afca
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-02-22 15:21:48 +0100

    ci: release_github.sh

--- scripts/release_dockerhub.sh
+++ scripts/release_dockerhub.sh
@@ -3,6 +3,6 @@ NC='\033[0m' # No Color
 
 printf "${GREEN}Release Dockerhub${NC}\n"
 
-echo ${DOCKER_PASS} | docker login --username=${DOCKER_USER} --password-stdin docker.io
 docker-compose -f infra/docker/docker-compose.yml build
+echo ${DOCKER_PASS} | docker login --username=${DOCKER_USER} --password-stdin docker.io
 docker-compose -f infra/docker/docker-compose.yml push
--- scripts/release_github.sh
+++ scripts/release_github.sh
@@ -3,13 +3,8 @@ NC='\033[0m' # No Color
 
 printf "${GREEN}Release Github${NC}\n"
 
-
-docker-compose -f infra/docker/docker-compose.yml build
-echo "${GITHUB_TOKEN}" | docker login docker.pkg.github.com -u ${{ github.actor }} --password-stdin
-REPO=$(echo "${{ github.repository }}" | tr '[:upper:]' '[:lower:]')
+REPO=$(echo $GITHUB_REPO | tr '[:upper:]' '[:lower:]')
 IMAGE_ID=docker.pkg.github.com/$REPO/$GITHUB_DOCKER_IMAGE_NAME
-VERSION=$(echo "${{ github.ref }}" | sed -e 's,.*/\(.*\),\1,')
-[[ "${{ github.ref }}" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
-[ "$VERSION" == "master" ] && VERSION=latest
-docker tag $DOCKERHUB_DOCKER_IMAGE_NAME $IMAGE_ID:$VERSION
-docker push $IMAGE_ID:$VERSION
+docker tag $DOCKERHUB_DOCKER_IMAGE_NAME $IMAGE_ID:latest
+echo $GITHUB_TOKEN | docker login docker.pkg.github.com -u $GITHUB_USER --password-stdin
+docker push $IMAGE_ID:latest
