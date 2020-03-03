86e19af8c50f628d2741e880eb7b5b842485e4aa
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-03-03 23:57:29 +0100

    chore: update to theme

--- src/client/web/theme.js
+++ src/client/web/theme.js
@@ -1,7 +1,13 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import green from '@material-ui/core/colors/green';
 
 // Create a theme instance.
 const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: green,
+  },
   background: {
     primary: '#eceff1',
   },
