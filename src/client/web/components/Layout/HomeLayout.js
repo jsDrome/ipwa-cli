fcae4044cf0daba5d00dba8f5e0511b55a640e21
Sreeram Padmanabhan <sreeram@jsdrome.com>
2020-03-03 20:53:18 +0100

    feat: modal

--- src/client/web/components/Layout/HomeLayout.js
+++ src/client/web/components/Layout/HomeLayout.js
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 
 import Navbar from '../Navbar/Navbar';
 import Toolbar from '../Toolbar/Toolbar';
+import Modal from '../Modal/Modal';
 import Sidebar from '../Sidebar/Sidebar';
 
 import './HomeLayout.less';
 
 const HomeLayout = ({ children }) => {
   const [ isSidebarOpen, setIsSidebarOpen ] = useState();
+  const [ isModalOpen, setModalOpen ] = useState(false);
 
   const appBarProps = {
     elevation: 0,
   };
 
   return <div className="HomeLayout">
-    <Navbar appBarProps={appBarProps} onMenuButtonClick={() => setIsSidebarOpen(!isSidebarOpen)}/>
+    <Modal
+      isModalOpen={isModalOpen}
+      handleModalClose={() => setModalOpen(false)} />
+    <Navbar
+      appBarProps={appBarProps}
+      onMenuButtonClick={() => setIsSidebarOpen(!isSidebarOpen)}
+      onRegisterClick={() => setModalOpen('register')} />
     <Toolbar />
     <Sidebar isDrawerOpen={isSidebarOpen} handleDrawerToggle={() => setIsSidebarOpen(!isSidebarOpen)} />
     <div className="HomeLayout_contents">
